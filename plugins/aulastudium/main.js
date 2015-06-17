aa5ea3b68727b865be13cb03699f00e77f0fe6e9
Raúl <raul2117@gmail.com>
2015-06-17 12:28:15 +0200

    2.0.1

--- plugins/aulastudium/main.js
+++ plugins/aulastudium/main.js
@@ -27,6 +27,17 @@ define(templates, function (theme, loginForm, program) {
         }
     };
 
+    // Inject allways our custom theme.
+    $("#mobilecssurl").html(theme);
+
+    // Replace the sign-up form with our custom template.
+    $("#add-site_template").html(loginForm);
+
+    // Automatically use the URL of the moodle moot without further checks.
+    MM.checkSite = function(e) {
+        MM.addSite(e);
+    };
+
 
 
 });
