8ee39fa783c09c84ee30870c4a59e24b826a2e8c
Raúl <raul2117@gmail.com>
2015-06-17 12:32:47 +0200

    2.0.2

--- plugins/aulastudium/main.js
+++ plugins/aulastudium/main.js
@@ -33,11 +33,18 @@ define(templates, function (theme, loginForm, program) {
     // Replace the sign-up form with our custom template.
     $("#add-site_template").html(loginForm);
 
-    // Automatically use the URL of the moodle moot without further checks.
-    MM.checkSite = function(e) {
-        MM.addSite(e);
+
+
+    // Inject allways our custom theme.
+    MM.loadCachedRemoteCSS = function(e) {
+        $("#mobilecssurl").html(theme);
     };
 
+    // Do not display the manage accounts page.
+    MM._displayManageAccounts = function() {
+        MM._displayAddSite();
+    };
 
+    MM.registerPlugin(plugin);
 
 });
